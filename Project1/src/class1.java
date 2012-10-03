1a75834f571476b6b179ce5aaf90b023c5d70760
Shivatrains <shiva.trains@yahoo.com>
2012-10-03 20:08:00 +0530

    second commit in java prog

---
+++ Project1/src/class1.java
@@ -0,0 +1,15 @@
+
+public class class1 {
+
+	/**
+	 * @param args
+	 */
+	
+	int a;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome to APEX");
+
+	}
+
+}
