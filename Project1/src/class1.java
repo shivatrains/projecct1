8f93493a02cfc9a7cd25b7b470d7e852a0cf9361
Shivatrains <shiva.trains@yahoo.com>
2012-10-04 02:55:28 +0530

    commint int veriables

--- Project1/src/class1.java
+++ Project1/src/class1.java
@@ -5,7 +5,7 @@ public class class1 {
 	 * @param args
 	 */
 	
-	int a;
+	int a; int b;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("welcome to APEX");
